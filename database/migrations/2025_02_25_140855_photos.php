ac4ee9fa725adfd666a46adb5b63041f79a61612
EduardoBrehm <edu.brehm13@gmail.com>
2025-02-25 12:09:24 -0300

    feat: adiciona migrations para capsulas e fotos

---
+++ database/migrations/2025_02_25_140855_photos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id('photo_id');
+            $table->foreignId('capsule_id')->constrained('capsules', 'id')->onDelete('cascade');
+            $table->string('name');
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
